366a2e9f054ee4c4f01983b3a1bee4e9ec3d15fc
kalibani <kalibani.ka@gmail.com>
2017-10-04 12:24:55 +0700

    contacts pake promise

--- models/contacts.js
+++ models/contacts.js
@@ -2,33 +2,63 @@ const sqlite3 = require('sqlite3').verbose();
 const db = new sqlite3.Database('db/data.db');
 
 class ModelContacts {
-  static findAll(callback){
-    db.all(`SELECT * FROM contacts`, (err, rowsContacts) =>{
-      callback(rowsContacts)
+  static findAll(){
+    return new Promise(function(resolve, reject) {
+      db.all(`SELECT * FROM contacts`, (err, rowsContacts) =>{
+        if (!err) {
+          resolve(rowsContacts)
+        }else {
+          reject(err)
+        }
+      });
     })
   }
 
-  static createContacts(column, callback){
-    db.run(`INSERT INTO contacts (name, company, telp_number, email) VALUES ('${column.name}','${column.company}','${column.telp_number}','${column.email}')`, (err)=>{
-      callback(err)
+  static createContacts(column){
+    return new Promise(function(resolve, reject) {
+      db.run(`INSERT INTO contacts (name, company, telp_number, email) VALUES ('${column.name}','${column.company}','${column.telp_number}','${column.email}')`, (err)=>{
+        if (!err) {
+          resolve()
+        }else {
+          reject(err)
+        }
+      })
     })
   }
 
-  static findByid(params, callback){
-    db.all(`SELECT * FROM contacts WHERE id = ${params}`, (err, rowsContacts)=>{
-      callback(err, rowsContacts)
+  static findByid(params){
+    return new Promise(function(resolve, reject) {
+      db.all(`SELECT * FROM contacts WHERE id = ${params}`, (err, rowsContacts)=>{
+        if (!err) {
+          resolve(rowsContacts)
+        }else {
+          reject(err)
+        }
+      })
     })
   }
 
-  static updateContacts(body, params, callback){
-    db.run(`UPDATE contacts SET name = '${body.name}', company = '${body.company}', telp_number = '${body.telp_number}', email='${body.email}' WHERE id = ${params}`, (err) => {
-      callback(err)
+  static updateContacts(body, params){
+    return new Promise(function(resolve, reject) {
+      db.run(`UPDATE contacts SET name = '${body.name}', company = '${body.company}', telp_number = '${body.telp_number}', email='${body.email}' WHERE id = ${params}`, (err) => {
+        if (!err) {
+          resolve()
+        }else {
+          reject(err)
+        }
+      })
     })
   }
 
-  static deleteContacts(params, callback){
-    db.run(`DELETE FROM contacts WHERE id = ${params}`,(err)=> {
-      callback(err)
+  static deleteContacts(params){
+    return new Promise(function(resolve, reject) {
+      db.run(`DELETE FROM contacts WHERE id = ${params}`,(err)=> {
+        if (!err) {
+          resolve()
+        }else {
+          reject(err)
+        }
+      })
     })
   }
 
--- routes/contacts.js
+++ routes/contacts.js
@@ -4,49 +4,52 @@ const router = express.Router();
 const modelContacts = require('../models/contacts');
 
 router.get('/', (req, res)=>{
-  modelContacts.findAll((rowsContacts)=>{
+  modelContacts.findAll()
+  .then(rowsContacts=>{
     res.render('contacts', { dataContacts : rowsContacts, title : 'Halaman Contacts'})
+  })
+  .catch(err =>{
+    res.send(err)
   })
 })
 
 router.post('/', (req, res)=>{
-  modelContacts.createContacts(req.body, (err)=>{
-    if (!err) {
-      res.redirect('/contacts')
-    }else {
-      res.send(err)
-    }
+  modelContacts.createContacts(req.body)
+  .then(()=>{
+    res.redirect('/contacts')
+  })
+  .catch(err=>{
+    res.send(err)
   })
 })
 
 router.get('/edit/:id', (req, res)=>{
-  modelContacts.findByid(req.params.id, (err, rowsContacts)=>{
-    if (!err) {
-      // res.send(rowsContacts)
-      res.render('contactsedit', {dataContacts: rowsContacts})
-    }else {
-      console.log(err)
-    }
+  modelContacts.findByid(req.params.id)
+  .then(rowsContacts=>{
+    res.render('contactsedit', {dataContacts: rowsContacts})
+  })
+  .catch(err=>{
+    res.send(err)
   })
 })
 
 router.post('/edit/:id', (req, res)=>{
-  modelContacts.updateContacts(req.body, req.params.id, (err)=>{
-    if (!err) {
-      res.redirect('/contacts')
-    }else {
-      res.send(err)
-    }
+  modelContacts.updateContacts(req.body, req.params.id)
+  .then(()=>{
+    res.redirect('/contacts')
+  })
+  .catch(err=>{
+    res.send(err)
   })
 })
 
 router.get('/delete/:id', (req, res)=>{
-  modelContacts.deleteContacts(req.params.id, (err)=>{
-    if (!err) {
+  modelContacts.deleteContacts(req.params.id)
+  .then(()=>{
       res.redirect('/contacts')
-    }else {
-      res.send(err)
-    }
+  })
+  .catch(err=>{
+    res.send(err)
   })
 })
 
